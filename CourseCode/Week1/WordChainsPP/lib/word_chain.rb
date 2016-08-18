c708ef09f8af8dedb67e6d112cd72d714cfa7223
Lillian Pham <lillian.l.pham@gmail.com>
2016-08-18 17:09:40 -0400

    Updated WordChainsPP Final

--- CourseCode/Week1/WordChainsPP/lib/word_chain.rb
+++ CourseCode/Week1/WordChainsPP/lib/word_chain.rb
@@ -9,47 +9,38 @@ class WordChain
     	}
     end
 
-    def next_letter(letter)
-    	(letter.ord + 1).chr
-    end
+    # def next_letter(letter)
+    # 	(letter.ord + 1).chr
+    # end
 
     def find_chain(word1, word2)
-   #  	while
-		 # if @dictionary.exists?(word1)
-		 # 	p word1
-		 # end
-		 # if @dictionary.exists?(word2)
-		 # 	p word2
-		 # end
 		 word1_array = split_word(word1)
 		 word2_array = split_word(word2)
 
-		 is_a_match = true
-		 
-		 while(is_a_match)
 		 	index = 0
-			 word1_array.each { |letter|
-			 	if letter == word2_array[index]
-			 		puts 'LETTER match'
-			 	else
-			 		word1_copy = word1
-			 		word1_copy[index] = next_letter(letter)
-			 		p "word1 copy"
-			 		if @dictionary.exists?(word1_copy)
-			 			puts word1_copy
-			 			p "word exists"
-			 			if word1_copy == word2
-			 				puts 'WORD match'
-			 				is_a_match == false
-			 			end
-			 		else
-			 			puts "not a word"
+
+			 word1_array.cycle { |letter|
+			 	letter_copy = word1[index]
+			 	#p index
+			 	word1[index] = word2[index]
+			 	p word1
+			 	if @dictionary.exists?(word1)
+			 		p "exists"
+			 		if word1 == word2
+			 			puts "MATCH"
+			 			break;
 			 		end
+				else
+					p "does NOT exist"
+					word1[index] = letter_copy
 			 	end
 			 	p index
-			 	index+=1
-			 }
-		end
+			  if index >= word1_array.length-1
+			  	index = 0
+			  else
+			  	index += 1
+			  end
 
+			 }
     end
 end
---
+++ CourseCode/Week1/module_exercises/lib/word_chain.rb
@@ -0,0 +1,11 @@
+class WordChain
+    def initialize(dictionary)
+        @dictionary = dictionary
+    end
+
+    def find_chain
+        # Write your algorithm here
+
+	 if the_dictionary.exists?(new_word)
+    end
+end
