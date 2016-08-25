cfa8ac9f9cabe6e7daa22e8f714bd387b637c668
Lillian Pham <lillian.l.pham@gmail.com>
2016-08-25 17:40:51 -0400

    Updated Sinatra Blog

--- CourseCode/Week2/SinatraBlog/app.rb
+++ CourseCode/Week2/SinatraBlog/app.rb
@@ -1,19 +1,49 @@
+#app.rb 
+
 require 'sinatra'
+require "sinatra/reloader" if development?
+
 require_relative("lib/blog.rb")
 require_relative("lib/post.rb")
 
+lillys_blog = Blog.new
+lillys_blog.add_post Post.new("Post 1", "16/7/10", "Hello, world. Welcome!")
+lillys_blog.add_post Post.new("Post 2", "16/7/1", "Here I am again!")
+lillys_blog.add_post Post.new("Post 3", "16/7/30", "You're still reading mah blog!")
+lillys_blog.add_post Post.new("Post 4", "16/8/1", "YAY!!!")
+lillys_blog.add_post Post.new("Post 5", "16/8/3", "Today is great")
+lillys_blog.add_post Post.new("Post 6", "16/8/5", "Who are you?")
+lillys_blog.add_post Post.new("Post 7", "16/8/7", "Hi again.")
+lillys_blog.add_post Post.new("Post 8", "16/8/10", "Love my blog?")
+lillys_blog.add_post Post.new("Post 9", "16/8/12", "Howdy")
+
+
 get '/' do 
-	@posts = lillys_blog.post
 	erb :home 
 end
 
-lillys_blog = Blog.new
-# lillys_blog.add_post(post1) Post.new("Post 1", "16/7/10", "Hello, world. Welcome!")
-# lillys_blog.add_post(post2) Post.new("Post 2", "16/7/1", "Here I am again!")
-# lillys_blog.add_post(post3) Post.new("Post 3", "16/7/30", "You're still reading mah blog!")
-# lillys_blog.add_post(post4) Post.new("Post 4", "16/8/1", "YAY!!!")
-# lillys_blog.add_post(post5) Post.new("Post 5", "16/8/3", "Today is great")
-# lillys_blog.add_post(post6) Post.new("Post 6", "16/8/5", "Who are you?")
-# lillys_blog.add_post(post7) Post.new("Post 7", "16/8/7", "Hi again.")
-# lillys_blog.add_post(post8) Post.new("Post 8", "16/8/10", "Love my blog?")
-# lillys_blog.add_post(post9) Post.new("Post 9", "16/8/12", "Howdy")
+get '/posts' do
+	@posts = lillys_blog.posts 
+
+	erb :posts
+end
+
+get '/create_new_post' do
+	erb :create_new_post
+end
+
+
+post '/submit_post' do
+	@posts = lillys_blog.posts
+	post = Post.new(params[:title], params[:date], params[:content])
+	@posts.push(post)
+
+	redirect to ("/posts")
+end
+
+get 'post/id' do 
+	erb :post_id
+end
+
+
+
--- CourseCode/Week2/SinatraBlog/spec/app_spec.rb
+++ CourseCode/Week2/SinatraBlog/spec/app_spec.rb
@@ -1,3 +1,4 @@
 require_relative '../app.rb'
 require 'rspec'
 require 'rack/test'
+
