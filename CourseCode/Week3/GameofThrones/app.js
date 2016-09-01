3b973a110983cb62eebff4a24adb5d30c9c15967
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-01 16:13:52 -0400

    Updated Game of Thrones Exercise

--- CourseCode/Week3/GameofThrones/app.js
+++ CourseCode/Week3/GameofThrones/app.js
@@ -8,25 +8,37 @@ function fileActions(err, file){
     var episodes = JSON.parse(file);
     console.log(episodes[0].title);
 
+    var findJonSnow = episodes.filter(function(ep){
+    	return ep.description.includes("Jon Snow")
 
-	episodes.forEach(function (eachEpisode) {
+    });
+
+	var filterLosers = findJonSnow.filter(function(ep) {
+		return ep.rating >= 8.5;
+
+	});
+
+	var eps = filterLosers.sort(function (a, b){
+		return a.episode_number - b.episode_number
+	});
+
+
+
+	filterLosers.forEach(function (eachEpisode) {
 		
 		eachEpisode.star = "*";
-		var numStars = eachEpisode.rating.round; 
+		var numStars = Math.round(eachEpisode.rating); 
 			while (eachEpisode.star.length < numStars) {
 				eachEpisode.star += "*";
-			console.log(eachEpisode.star);
 			};
-
 
 		console.log(`Title: ${eachEpisode.title}  Episode: ${eachEpisode.episode_number}`);
 		console.log(eachEpisode.description);
 		console.log(`Rating: ${eachEpisode.rating} ${eachEpisode.star} \n`);
 
 	});
-
-
 }
 
 fs.readFile("./GoTEpisodes.json", 'utf8', fileActions);
+
 
