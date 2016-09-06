fb99c26941d0ad3bd78af9c1e9c745974d5def38
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-06 18:33:30 -0400

    Update word_central with word count

--- CourseCode/Week4/word_central/app/controllers/concerns/text_inspections_controller.rb
+++ CourseCode/Week4/word_central/app/controllers/concerns/text_inspections_controller.rb
@@ -6,7 +6,9 @@ class TextInspectionsController < ApplicationController
 	def create
 		@text = params[:text_inspection][:user_text].inspect 
 
-		@word_count = @text.split("").length
+		@word_count = @text.split.length
+
+		@count = @word_count/275
 
 		render "results"
 	end
--- CourseCode/Week4/word_central/config/routes.rb
+++ CourseCode/Week4/word_central/config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 	get '/text_inspections/new', to: 'text_inspections#new'
 
 	post '/text_inspections', to: 'text_inspections#create'
+
+	get 'text_inspections/results', to: 'text_inspections#results'
+
 end
