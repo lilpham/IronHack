21b688afda32664c2452d68ed8fa2f684cf38ce5
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-08 11:20:10 -0400

    updated form in new.html.erb

--- CourseCode/Week4/timetracking/app/controllers/projects_controller.rb
+++ CourseCode/Week4/timetracking/app/controllers/projects_controller.rb
@@ -4,10 +4,25 @@ class ProjectsController < ApplicationController
 		
 		render "index"
 	end 
+
 	def show
 		@project = Project.find_by(id: params[:id])
 		unless @project
 			render "no_project_found"
 		end
+	end
+
+	def new
+		@project = Project.new 
+	end 
+
+	def create
+		project = Project.new(
+			name: params[:project][:name],
+			description: params[:project][:description])
+
+		project.savecl
+
+		redirect_to("/projects/@project.id")
 	end
 end
