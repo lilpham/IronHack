73da562c178892a8cc41c2b4dacb722b6aafe117
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-09 18:04:17 -0400

    Update Create Products migration

--- CourseCode/Week4/auction_site/db/migrate/20160909214920_create_products.rb
+++ CourseCode/Week4/auction_site/db/migrate/20160909214920_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
+      t.references :user, foreign_key: true
       t.string :title
       t.text :description
       t.datetime :deadline
-      t.references :products, foreign_key: true
     end
   end
 end
--- CourseCode/Week4/auction_site/db/schema.rb
+++ CourseCode/Week4/auction_site/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909214037) do
+ActiveRecord::Schema.define(version: 20160909214920) do
 
   create_table "products", force: :cascade do |t|
+    t.integer  "user_id"
     t.string   "title"
     t.text     "description"
     t.datetime "deadline"
-    t.integer  "products_id"
-    t.index ["products_id"], name: "index_products_on_products_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
