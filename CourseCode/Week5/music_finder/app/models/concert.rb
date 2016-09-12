294e0fb1b584c4cc113713e1c89cca8abbd5b53f
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-12 10:34:14 -0400

    Create Concert actions

--- CourseCode/Week5/music_finder/app/models/concert.rb
+++ CourseCode/Week5/music_finder/app/models/concert.rb
@@ -1,2 +1,10 @@
 class Concert < ApplicationRecord
+	def show
+	end
+
+	def new
+	end 
+
+	def create
+	end 
 end
--- CourseCode/Week5/music_finder/config/routes.rb
+++ CourseCode/Week5/music_finder/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-	resources :concerts, only: [:new, :create, :show]
+	resources :concerts, only: [:show, :new, :create]
 end
