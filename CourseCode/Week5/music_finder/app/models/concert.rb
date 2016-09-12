cf6c00f5e3ada9aef27d7c3ccb806af8850bac29
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-12 10:43:08 -0400

    Add validations to concert model

--- CourseCode/Week5/music_finder/app/models/concert.rb
+++ CourseCode/Week5/music_finder/app/models/concert.rb
@@ -1,3 +1,7 @@
 class Concert < ApplicationRecord
-	
+	validates :artist, :venue, :city, :date,
+	 :price, :description, presence: true 
+	validates :date, :price, numericality: true 
+	validates :city, :description, format: { :with => /\A[a-zA-Z]+\z/,
+		      :message => "Only letters allowed" }
 end
