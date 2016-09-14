9779f3ea41b0543963ef551602f114b536658035
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-14 10:46:56 -0400

    Add function to app.js

--- CourseCode/Week5/ajax/app.js
+++ CourseCode/Week5/ajax/app.js
@@ -1,1 +1,12 @@
 console.log("We are WERQin");
+
+$(document).ready(function(){
+	$(".js-fetch-characters").on("click", fetchCharacters);
+
+});
+
+function fetchCharacters () {
+	console.log("Fetch Characters Click");
+
+	//AJAX CODE HERE
+}
