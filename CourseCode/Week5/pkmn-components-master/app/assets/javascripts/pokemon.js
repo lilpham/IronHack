3961484856863b8c9c4ee57a508edf528e4083b3
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-15 10:38:37 -0400

    update pokemon.js

--- CourseCode/Week5/pkmn-components-master/app/assets/javascripts/pokemon.js
+++ CourseCode/Week5/pkmn-components-master/app/assets/javascripts/pokemon.js
@@ -1,2 +1,24 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+PokemonApp.PokemonComponent = class {
+	constructor (pokemonUri) {
+		this.id = PokemonApp.idFromUri(pokemonUri);
+	}
+
+	render () {
+		console.log(`PokemonComponent renderin pokemon #${this.id}`");
+	}
+};
+
+// Input: "v1/pokemon/57"
+// Output: 
+PokemonApp.idFromUri = function (pokemonUri) {
+	var uriSegments = pokemonUri.split("/");
+	var secondLast = uriSegments.length - 2;
+	return uriSegments[secondLast];
+};
+
+$(document).ready(function (){
+	$(".js-show-pokemon").on("click", function (){
+		console.log("CLICK");
+	});
+
+});
