014742ae60bd20a95fba1f6a149a23fcfcb37bc4
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-15 11:39:23 -0400

    Update pokemon exercise

--- CourseCode/Week5/pkmn-components-master/app/assets/javascripts/pokemon.js
+++ CourseCode/Week5/pkmn-components-master/app/assets/javascripts/pokemon.js
@@ -4,8 +4,27 @@ PokemonApp.PokemonComponent = class {
 	}
 
 	render () {
-		console.log(`PokemonComponent renderin pokemon #${this.id}`");
+		console.log(`PokemonComponent render in pokemon #${this.id}`);
+
+		$.ajax ({	
+			type: "GET",
+			url: `api/v1/pokemon/${this.id}`,
+			success: PokemonApp.showPokemonModal,
+			error: PokemonApp.handleError
+		});
 	}
+};
+
+PokemonApp.showPokemonModal = function (result) {
+	console.log("Pokemon Info:");
+	console.log(result);
+
+	$(".js-pokemon-modal").modal("show");
+};
+
+PokemonApp.handleError = function (theError) {
+	console.log("Pokemon component error");
+	console.log(theError.responseText);
 };
 
 // Input: "v1/pokemon/57"
@@ -17,8 +36,15 @@ PokemonApp.idFromUri = function (pokemonUri) {
 };
 
 $(document).ready(function (){
-	$(".js-show-pokemon").on("click", function (){
+	$(".js-show-pokemon").on("click", function (theEvent){
 		console.log("CLICK");
+		
+	// date-pokemon-uri="api/v1/pokemon/57"
+
+	var theUri = $(theEvent.currentTarget).data("pokemon-uri"); 
+
+	var pokeComponent = new PokemonApp.PokemonComponent( theUri );
+		pokeComponent.render();
 	});
 
 });
