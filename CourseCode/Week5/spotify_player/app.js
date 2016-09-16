380b7e61eec37d069ae671e4bd52d1ff070559ea
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-16 15:47:23 -0400

    update spotify player with track info

--- CourseCode/Week5/spotify_player/app.js
+++ CourseCode/Week5/spotify_player/app.js
@@ -20,10 +20,33 @@ function searchTrack (theEvent) {
 }
 
 function showResult (response) {
-	console.log(respone);
+
+	var result = response.tracks.items[0];
+	console.log(result);
+
+	console.log("Track title:");
+	console.log(result.name)
+
+	$(".js-track-title").html(result.name);
+
+	console.log("Artist Name:");
+	console.log(result.artists[0].name);
+
+	$(".js-artist-name").html(result.artists[0].name);
+
+	console.log("Album Image:");
+
+	$(".js-cover-img").attr("src",result.album.images[0].url);
+
+	console.log(result.preview_url);
+	$(".js-audio").attr("src",result.album.preview_url);
+
 }
 
 function handleError (error) {
 	console.log("Error");
 	console.log(error.ResponseText);
 }
+
+
+
