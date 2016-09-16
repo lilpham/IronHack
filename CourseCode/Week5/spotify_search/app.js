4dca81ffb2af92314d8c2a0a704b1a322daf1d62
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-16 10:04:05 -0400

    Update spotify

--- CourseCode/Week5/spotify_search/app.js
+++ CourseCode/Week5/spotify_search/app.js
@@ -1,6 +1,8 @@
 $(document).ready (function () {
 	$(".js-search").on("click", search);
 	$(".js-album-search").on("click", fetchAlbums);
+	$(".js-album-tracks").on("click", fetchTracks);
+
 });
 
 function search (theEvent) {
@@ -16,6 +18,7 @@ function search (theEvent) {
 		success: showArtists,
 		error: handleError	
 	});
+
 }
 
 function showArtists (response) {
@@ -65,6 +68,8 @@ function fetchAlbums (theEvent) {
 function showAlbums (response) {
 	console.log("Album Info:");
 	console.log(response.items);
+	$(".js-album-list").empty( );
+
 
 	var album = response.items;
 
@@ -72,7 +77,8 @@ function showAlbums (response) {
 		if (theAlbum.images[0]) {
 				var html =
 				`<li>
-					<h3> "${theAlbum.name}" </h3>
+					<button class = "js-album-tracks" data-albumid = "${theAlbum.id}">
+					<h3> "${theAlbum.name}" </h3> </button>
 					<br>			
 					<img src = ${theAlbum.images[1].url}>
 				</li>`;
@@ -83,6 +89,43 @@ function showAlbums (response) {
 	$(".js-album-modal").modal("show");
 
 }
+
+
+
+function fetchTracks (theEvent) {
+
+	console.log("Track List Click");
+
+	var albumId = $(theEvent.currentTarget).data("albumid");
+
+	$.ajax({
+		type: "GET",
+		url: `https://api.spotify.com/v1/albums/${albumid}/tracks`,
+		success: showTracks,
+		error: handleError	
+	});
+}
+
+function showTracks (response) {
+	console.log("Track Info:");
+	console.log(response);
+
+	// var tracks = response;
+
+	// tracks.forEach(function (theTrack) {
+	// 	var html =
+	// 	`<li>
+	// 		<h4> "${theTrack}" </h4>
+	// 		<br>			
+	// 	</li>`;
+	// 	$(".js-track-list").append(html);
+	// });
+
+	// $(".js-track-modal").modal("show");
+}
+
+
+
 
 
 
