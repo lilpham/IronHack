1b7c08722e25123a15644468ae31242aa4bd2ed9
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-16 10:07:01 -0400

    Create Pizza Builder

--- CourseCode/Week5/pizza-builder-master/src/pizza.js
+++ CourseCode/Week5/pizza-builder-master/src/pizza.js
@@ -1,17 +1,87 @@
 // Write your Pizza Builder JavaScript in this file.
-$('.btn-pepperonni').on('click', function () {
-	$('.pep').toggle();
-});
+$(document).ready(function(){
 
-$('.btn-pepperonni').on('click', function () {
-	$('.pep').toggle();
-});
+var price = 21
+// $('.total').html("$" + price);
 
-$('.btn-pepperonni').on('click', function () {
-	$('.pep').toggle();
-});
+// $(".btn").on('click', function(){
+// 	$('.total').html("$" + price);
+// });
+$('.total').html("$" + price);
+	$('.btn-pepperonni').on('click', function(){
+		$('.pep').toggle();
+		$('.btn-pepperonni').toggleClass('active')
+			if ($('.btn-pepperonni').hasClass('active')) {	
+				 price += 1;
+				 $('.lipep').show();
+				 
+		}	else{
+				price -= 1;
+				$('.lipep').hide();
+		};
+		$('.total').html("$" + price);		
+	});
 
-$('.btn-pepperonni').on('click', function () {
-	$('.pep').toggle();
+
+	$('.btn-mushrooms').on('click', function(){
+		$('.mushroom').toggle();
+		$('.btn-mushrooms').toggleClass('active');
+			if ($('.btn-mushrooms').hasClass('active')) {	
+				 price += 1;
+				 $('.limush').show();
+				 
+		}	else{
+				price -= 1;
+				$('.limush').hide();
+		};
+		$('.total').html("$" + price)	
+	});
+
+	$('.btn-green-peppers').on('click', function(){
+		$('.green-pepper').toggle();
+			$('.btn-green-peppers').toggleClass('active');
+			if ($('.btn-green-peppers').hasClass('active')) {	
+				 price += 1;
+				 $('.ligreen').show();
+				 
+		}	else{
+				price -= 1;
+				$('.ligreen').hide();
+		};
+		$('.total').html("$" + price)	
+	});
+
+	$('.btn-sauce').on('click', function(){
+		$('.sauce').toggleClass('sauce-white');
+		$('.btn-sauce').toggleClass('active');
+			if ($('.btn-sauce').hasClass('active')) {	
+				 price += 3;
+				 $('.liwhite').show();
+				 
+		}	else{
+				price -= 3;
+				$('.liwhite').hide();
+		};
+		$('.total').html("$" + price)	
+	});
+
+	$('.btn-crust').on('click', function(){
+		$('.crust').toggleClass('crust-gluten-free');
+		$('.btn-crust').toggleClass('active');
+			if ($('.btn-crust').hasClass('active')) {	
+				 price += 5;
+				 $('.ligluten').show();
+				 
+		}	else{
+				price -= 5;
+				$('.ligluten').hide();
+		};	
+		$('.total').html("$" + price)
+	});
+//When pepperonni button is clicked $1 is added to price ($10) and changes price to $11.
+
+// var price = 10
+$('.total').html("$" + price);
+
 });
 
