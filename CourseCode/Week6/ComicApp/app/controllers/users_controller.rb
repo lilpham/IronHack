2e2ebb5650bb8edcdd0f7df0f74dd7eca56db28b
Lillian Pham <lillian.l.pham@gmail.com>
2016-09-21 15:16:08 -0400

    final authentication update to comic app

--- CourseCode/Week6/ComicApp/app/controllers/users_controller.rb
+++ CourseCode/Week6/ComicApp/app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!, only: [:show]
+
 	def home
+		@users = User.all
 		render :home 
 	end 
 
+	def show
+		@user = User.find(params[:id])
+		render :show 
+	end 
 end
